ca0c3ede68deb630143d73dec5b9b63c8eb61c8f
Miharisoa Rakotonirina <miharisoa.rakotonirina.06@gmail.com>
2025-12-10 14:55:25 +0300

    feat(contact_form): add zod validation, field error handling and touched-based UX

--- components/contact_form.tsx
+++ components/contact_form.tsx
@@ -5,6 +5,7 @@ import { Input } from "./ui/input";
 import { Textarea } from "./ui/textarea";
 import { Button } from "./ui/button";
 import { toast } from "sonner";
+import contactScheme from "@/lib/validation/contact_scheme";
 
 export default function ContactForm() {
   const [informations, setInformations] = React.useState({
@@ -13,6 +14,28 @@ export default function ContactForm() {
     subject: "",
     message: "",
   });
+
+  const [errors, setErrors] = React.useState<Record<string, string>>({});
+  const [isValid, setIsValid] = React.useState(false);
+  const [touched, setTouched] = React.useState<Record<string, boolean>>({});
+
+  React.useEffect(() => {
+    const result = contactScheme.safeParse(informations);
+
+    if (result.success) {
+      setErrors({});
+      setIsValid(true);
+    } else {
+      const newErrors: Record<string, string> = {};
+      result.error.issues.forEach((err) => {
+        if (err.path[0]) {
+          newErrors[err.path[0] as string] = err.message;
+        }
+      });
+      setErrors(newErrors);
+      setIsValid(false);
+    }
+  }, [informations]);
 
   const handleChange = (
     e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
@@ -24,8 +47,30 @@ export default function ContactForm() {
     }));
   };
 
+  const handleBlur = (
+    e: React.FocusEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name } = e.target;
+    setTouched((prevState) => ({
+      ...prevState,
+      [name]: true,
+    }));
+  };
+
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+
+    const parseFormResult = contactScheme.safeParse(informations);
+
+    if (!parseFormResult.success) {
+      toast.error("Please fill the form correctly");
+      setTouched({
+        fullName: true,
+        emailAddress: true,
+        subject: true,
+        message: true,
+      });
+    }
 
     const formData = new FormData(event.target as HTMLFormElement);
 
@@ -72,56 +117,92 @@ export default function ContactForm() {
         <div className="space-y-2 flex-1 min-w-0">
           <Label>Name</Label>
           <Input
+            onBlur={handleBlur}
             type="text"
-            className="w-full"
+            className={`w-full ${
+              touched.fullName && errors.fullName ? "border-destructive" : ""
+            }`}
             placeholder={"Your name"}
             onChange={handleChange}
             required={true}
             name="fullName"
             value={informations.fullName}
           />
+          <div className="min-h-2">
+            {touched.fullName && errors.fullName && (
+              <p className="text-sm text-destructive">{errors.fullName}</p>
+            )}
+          </div>
         </div>
 
         <div className="space-y-2 flex-1 min-w-0">
           <Label>Email</Label>
           <Input
+            onBlur={handleBlur}
             type="email"
-            className="w-full"
+            className={`w-full ${
+              touched.emailAddress && errors.emailAddress
+                ? "border-destructive"
+                : ""
+            }`}
             placeholder={"your.email@example.com"}
             onChange={handleChange}
             required={true}
             name="emailAddress"
             value={informations.emailAddress}
           />
+          <div className="min-h-2">
+            {touched.emailAddress && errors.emailAddress && (
+              <p className="text-sm text-destructive">{errors.emailAddress}</p>
+            )}
+          </div>
         </div>
       </div>
 
       <div className="space-y-2">
         <Label>Subject</Label>
         <Input
+          onBlur={handleBlur}
           type="text"
-          className="w-full"
+          className={`w-full ${
+            touched.subject && errors.subject ? "border-destructive" : ""
+          }`}
           placeholder="What would you like to discuss?"
           onChange={handleChange}
           required={true}
           name="subject"
           value={informations.subject}
         />
+        <div className="min-h-2">
+          {touched.subject && errors.subject && (
+            <p className="text-sm text-destructive">{errors.subject}</p>
+          )}
+        </div>
       </div>
 
       <div className="space-y-2">
         <Label>Message</Label>
         <Textarea
-          className="w-full resize-none h-52"
+          className={`w-full resize-none h-40  ${
+            touched.message && errors.message ? "border-destructive" : ""
+          }`}
           placeholder={"Tell me about your project or just say 👋"}
           onChange={handleChange}
           required={true}
           name="message"
           value={informations.message}
+          onBlur={handleBlur}
         />
+        <div className="min-h-2">
+          {touched.message && errors.message && (
+            <p className="text-sm text-destructive">{errors.message}</p>
+          )}
+        </div>
       </div>
 
-      <Button className="w-full cursor-pointer">Send Message</Button>
+      <Button className="w-full cursor-pointer" disabled={!isValid}>
+        Send Message
+      </Button>
     </form>
   );
 }
