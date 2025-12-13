c73fb0a79f35b0d8b6b4356cdb374ae17e82c9fa
Miharisoa Rakotonirina <miharisoa.rakotonirina.06@gmail.com>
2025-12-13 17:31:46 +0300

    feat(home): add preference button to toggle theme and language options

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,12 @@ import Project from "@/components/projects";
 import Skills from "@/components/skills";
 import Contact from "@/components/contact";
 import Footer from "@/components/footer";
+import PreferenceButton from "@/components/preference_button";
 
 export default function Home() {
   return (
     <div className="min-h-screen">
+      <PreferenceButton />
       <NavigationBar />
       <HeroSection />
       <About />
