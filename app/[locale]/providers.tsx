1e18081710873ce14084d2e831903e7e666d27c0
Miharisoa Rakotonirina <miharisoa.rakotonirina.06@gmail.com>
2025-12-18 13:30:44 +0300

    feat(locale): Add locale provider using next-international for translation management

---
+++ app/[locale]/providers.tsx
@@ -0,0 +1,12 @@
+import { I18nProviderClient } from "@/locales/client";
+import { PropsWithChildren } from "react";
+
+export default function Providers(
+  props: PropsWithChildren<{ locale: string }>
+) {
+  return (
+    <I18nProviderClient locale={props.locale}>
+      {props.children}
+    </I18nProviderClient>
+  );
+}
