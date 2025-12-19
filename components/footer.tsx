d2329637f36c5598d4a0d650d52476b844dfea0a
Miharisoa Rakotonirina <miharisoa.rakotonirina.06@gmail.com>
2025-12-19 11:42:06 +0300

    feat(i18n): add localization support in Footer component

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,19 +1,20 @@
+import { getI18n } from "@/locales/server";
 import Link from "next/link";
 
-export default function Footer() {
+export default async function Footer() {
+  const t = await getI18n();
+
   return (
     <div className="bg-muted/50 border-t border-border py-8">
       <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="text-center">
-          <p className="text-muted-foreground">
-            © 2025 John Doe. Built with React, Next.js, ShadCN UI and Tailwind
-            CSS.
-          </p>
+          <p className="text-muted-foreground">{t("footer.description")}</p>
           <Link
             className="text-muted-foreground text-sm pt-4"
             href={"https://whois-radiantwizard.vercel.app/"}
+            target="_blank"
           >
-            Inspired by Radiant-Wizard , thanks to him 🙏
+            {t("footer.inspiration")}
           </Link>
         </div>
       </div>
