afe78acd6866c2e23dc30ec7a0319a6db25f3182
Miharisoa Rakotonirina <miharisoa.rakotonirina.06@gmail.com>
2025-12-19 17:17:15 +0300

    refactor(validation): update contact schema to support dynamic and localized error messages

--- lib/validation/contact_scheme.ts
+++ lib/validation/contact_scheme.ts
@@ -1,10 +1,18 @@
 import * as zod from "zod";
 
-const contactScheme = zod.object({
-  fullName: zod.string().min(5, "At least 5 characters"),
-  emailAddress: zod.string().email("Invalid email address"),
-  subject: zod.string().min(10, "At least 10 characters"),
-  message: zod.string().min(30, "At least 30 characters"),
-});
-
-export default contactScheme;
+export const createContactScheme = (t: (...args: any[]) => string) => {
+  return zod.object({
+    fullName: zod
+      .string()
+      .min(5, t("contact.contactForm.fields.errors.fullName")),
+    emailAddress: zod
+      .string()
+      .email(t("contact.contactForm.fields.errors.emailAddress")),
+    subject: zod
+      .string()
+      .min(10, t("contact.contactForm.fields.errors.subject")),
+    message: zod
+      .string()
+      .min(30, t("contact.contactForm.fields.errors.message")),
+  });
+};
