dcd852826fbd9c4c10bb8eb1cf36b508932aa0db
Miharisoa Rakotonirina <miharisoa.rakotonirina.06@gmail.com>
2025-12-19 11:57:28 +0300

    feat(i18n): add localization support for ContactResume component, dynamically updating CV download link and button text based on the selected language (English/French).

--- components/contact_resume.tsx
+++ components/contact_resume.tsx
@@ -1,22 +1,35 @@
+"use client";
+import { useCurrentLocale, useI18n } from "@/locales/client";
 import { Button } from "./ui/button";
 import { Card, CardContent } from "./ui/card";
 import { Download } from "lucide-react";
 
 export default function ContactResume() {
+  const t = useI18n();
+  const locale = useCurrentLocale();
+
   return (
     <Card>
       <CardContent>
         <div className="text-center">
-          <h4 className="mb-2">Want to check out my resume?</h4>
+          <h4 className="mb-2">{t("contact.resume.title")}</h4>
           <a
-            href="/Miharisoa_Rakotonirina_CV.pdf"
-            download={"Miharisoa_Rakotonirina_CV"}
+            href={
+              locale === "en"
+                ? "Miharisoa_Rakotonirina_CV_EN.pdf"
+                : "Miharisoa_Rakotonirina_CV_FR.pdf"
+            }
+            download={
+              locale === "en"
+                ? "Miharisoa_Rakotonirina_CV_EN"
+                : "Miharisoa_Rakotonirina_CV_FR"
+            }
           >
             <Button variant={"outline"} className="cursor-pointer">
               <span>
                 <Download />
               </span>
-              <span>Download my CV</span>
+              <span>{t("contact.resume.buttonContent")}</span>
             </Button>
           </a>
         </div>
